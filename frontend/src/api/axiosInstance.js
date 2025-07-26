5efd5daeff45927d04200b6668e99b203a8bd603
AhmadFauzanZW <ahfa201204@gmail.com>
2025-07-26 21:20:15 +0700

    deploy preparation --> changed API_BASE_URL to `http://api.absenproyek.biz.id`

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = `http://localhost:5000/api`;
+// const API_BASE_URL = `http://localhost:5000/api`;
+const API_BASE_URL = `http://api.absenproyek.biz.id`;
 
 const axiosInstance = axios.create({
     // baseURL: 'https://api.absenproyek.biz.id/api', // Use HTTPS for API
